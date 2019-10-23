71dd71a403439eb60a16c1a2d2c4c17916bcbc0b
Benefrancis do Nascimento (PRR3) <benefrancis@mpf.mp.br>
2019-10-23 13:52:41 -0300

    Commit inicial

---
+++ src/main/java/br/com/benefrancis/deliveryauthserver/DeliveryAuthServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.benefrancis.deliveryauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/benefrancis/deliveryauthserver/DeliveryAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.benefrancis.deliveryauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
